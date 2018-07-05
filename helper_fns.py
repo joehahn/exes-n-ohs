5b055a0c7e29655c9d724e0667a84c427e68382f
Joe Hahn <joe.hahn@oracle.com>
2018-07-05 16:31:51 -0500

    helpers

--- helper_fns.py
+++ helper_fns.py
@@ -7,25 +7,25 @@
 #these helper functions are called by something.ipynb
 
 #do the following before importing this 
+#import numpy as np
 #rn_state = np.random.RandomState(seed=rn_seed)
 
 #generate a dict containing a single record's facts: id, coordinates, class etc
 import numpy as np
 def make_xo_dict(x_half_width, radius, box_half_width, jitter, id=-1):
     #get dot's cartesian coordinates
-    x = random.uniform(low=-box_half_width, high=box_half_width)
-    y = random.uniform(low=-box_half_width, high=box_half_width)
+    x = np.random.uniform(low=-box_half_width, high=box_half_width)
+    y = np.random.uniform(low=-box_half_width, high=box_half_width)
     #get dot's class
     dot_class = 'B'
-    import numpy.sqrt as sqrt
-    r = sqrt(x**2 + y**2)
+    r = np.sqrt(x**2 + y**2)
     if (r < radius):
         dot_class = 'O'
     if (np.abs(x) < x_half_width) or (np.abs(y) < x_half_width):
         dot_class = 'X'
     #rotate coordinate system by 45 degrees
     rot_angle = np.pi/4.0
-    c = cos(rot_angle)
+    c = np.cos(rot_angle)
     s = np.sin(rot_angle)
     xr =  x*c + y*s
     yr = -x*s + y*c
